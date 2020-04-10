b878f14279d9100314a4f5e4f9a4c6210e786456
Vitor Nagata <nagatavit@gmail.com>
2020-04-10 12:21:41 -0300

    Move some configs to programming utils

--- lisp/init-productivity.el
+++ lisp/init-productivity.el
@@ -67,33 +67,5 @@
   (interactive)
   (ansi-color-apply-on-region (point-min) (point-max)))
 
-(use-package rainbow-delimiters
-  :ensure t
-  :init
-  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
-
-(defun show-trailing-whitespace ()
-  "Enable display of trailing whitespace in this buffer."
-  (setq-local show-trailing-whitespace t))
-
-(dolist (hook '(prog-mode-hook text-mode-hook conf-mode-hook))
-  (add-hook hook 'show-trailing-whitespace))
-
-(setq kill-ring-max 100)
-
-(use-package yasnippet
-  :ensure t
-  :config
-  (use-package yasnippet-snippets
-    :ensure t)
-  (yas-reload-all)
-  :init
-  (yas-global-mode 1))
-
-;; Use nabla instead of TAB
-(define-key yas-minor-mode-map (kbd "<tab>") nil)
-(define-key yas-minor-mode-map (kbd "TAB") nil)
-(global-set-key '[8711] 'yas-expand)
-
 (provide 'init-productivity)
 ;;; init-productivity.el ends here
