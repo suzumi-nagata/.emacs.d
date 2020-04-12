0e48fa186319d5807f086af8768472b04af99383
Vitor Nagata <nagatavit@gmail.com>
2020-04-12 20:26:23 -0300

    Change org bullet to look nicer on input font

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -49,7 +49,7 @@
   :ensure t
   :init
   (setq org-bullets-bullet-list
-        '("⬢" "✤" "○" "◉" "✧" "♠" "♣" "♥" "♦"))
+        '("⬢" "✤" "◉" "❄" "✧" "♠" "♣" "♥" "♦"))
   :config
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
