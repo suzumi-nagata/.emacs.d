a58bebabf791e4afc5764965414e7af62f82433e
Vitor Nagata <nagatavit@gmail.com>
2020-04-21 11:21:11 -0300

    Remove company irony as I will use lsp instead

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -2,9 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-(add-hook 'c++-mode-hook 'yas-minor-mode)
-(add-hook 'c-mode-hook 'yas-minor-mode)
-
 (use-package ccls
   :ensure t
   :hook ((c-mode c++-mode objc-mode cuda-mode) .
@@ -13,26 +10,6 @@
 (add-hook 'c-mode-common-hook
   (lambda()
     (local-set-key  (kbd "M-o") 'ff-find-other-file)))
-
-;; (use-package company-irony
-;;   :ensure t
-;;   :config
-;;   (require 'company)
-;;   (add-to-list 'company-backends 'company-irony))
-
-;; (use-package company-c-headers
-;;   :ensure t)
-
-;; (use-package irony
-;;   :ensure t
-;;   :config
-;;   (add-hook 'c++-mode-hook 'irony-mode)
-;;   (add-hook 'c-mode-hook 'irony-mode)
-;;   (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
-
-;; (with-eval-after-load 'company
-;;   (add-hook 'c++-mode-hook 'company-mode)
-;;   (add-hook 'c-mode-hook 'company-mode))
 
 (provide 'init-c)
 ;;; init-c.el ends here
