eaee5a3adb69335bbd9ed100bb55427f0e0660d4
Suzumi Nagata <suzumi.nag@gmail.com>
2020-04-25 18:13:12 -0300

    Fix company keybinding

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -14,7 +14,7 @@
   (define-key company-active-map (kbd "M-n") nil)
   (define-key company-active-map (kbd "M-p") nil)
   (define-key company-active-map (kbd "C-n") #'company-select-next)
-  (define-key company-active-map (kbd "C-p") #'company-select-next))
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
 
 (add-hook 'emacs-lisp-mode-hook 'company-mode)
 
