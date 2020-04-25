d21d17348087ecf5799fae36a865bfdaeb659b6c
Vitor Nagata <nagatavit@gmail.com>
2020-04-24 23:50:28 -0300

    Modify some org stuff

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -6,15 +6,15 @@
   :ensure t
   :config
   (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 2))
+  (setq company-minimum-prefix-length 2)
+  (setq company-dabbrev-downcase nil))
 
 ;; Remap company navigation keys
 (with-eval-after-load 'company
   (define-key company-active-map (kbd "M-n") nil)
   (define-key company-active-map (kbd "M-p") nil)
   (define-key company-active-map (kbd "C-n") #'company-select-next)
-  (define-key company-active-map (kbd "C-p") #'company-select-next)
-  (define-key company-active-map (kbd "SPC") #'company-abort))
+  (define-key company-active-map (kbd "C-p") #'company-select-next))
 
 (add-hook 'emacs-lisp-mode-hook 'company-mode)
 
