6572f860f0ba325dc9e9fd3a9b76a74c91d7eec8
paulocesarvasco <paulocvasco.93@gmail.com>
2021-05-20 12:37:42 -0300

    Add lsp java

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -22,5 +22,10 @@
 (use-package dap-mode
   :ensure t)
 
+(use-package lsp-java
+  :ensure t
+  :config (add-hook 'java-mode-hook 'lsp))
+(setq lsp-java-jdt-download-url  "https://download.eclipse.org/jdtls/milestones/0.57.0/jdt-language-server-0.57.0-202006172108.tar.gz")
+
 (provide 'init-lsp)
 ;;; init-lsp.el ends here
