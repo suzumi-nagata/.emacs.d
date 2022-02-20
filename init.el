63ad1b29179a2ed3da4cd9d0c65f9444621169ce
Vitor Nagata <nagatavit@gmail.com>
2022-02-19 21:52:54 -0300

    Add blamer (finally some git lens equivalent 😮)

--- init.el
+++ init.el
@@ -454,6 +454,21 @@
   :config
   (global-diff-hl-mode))
 
+(use-package blamer
+  :straight t
+  :bind (("s-c" . blamer-show-commit-info))
+  :defer 20
+  :custom
+  (blamer-idle-time 0.3)
+  (blamer-min-offset 70)
+  :custom-face
+  (blamer-face ((t :foreground "#7a88cf"
+                    :background nil
+                    :height 100
+                    :italic t)))
+  :config
+  (global-blamer-mode 1))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
                                         ;           end productivity          ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
